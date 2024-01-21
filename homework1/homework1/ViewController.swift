5d9f40f1eeb7bc06c329d436db93cb56af2e03b2
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-01-21 18:18:53 +0630

    Day5 : UI Scroll View and Stack View

---
+++ homework1/homework1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework1
+//
+//  Created by Aye Chan Htun Naing on 21/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
